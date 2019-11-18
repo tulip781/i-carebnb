76680b4f344acea3ad288b745966aaca62d633e7
Ben Barton <benrussellbarton@gmail.com>
2019-11-18 17:15:40 +0000

    work in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
-  get 'rooms/new'
-  get 'rooms/index'
-  get 'rooms/show'
-  get 'rooms/create'
-  get 'rooms/edit'
-  get 'rooms/update'
-  get 'rooms/destroy'
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/create'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'bookings/destroy'
-  get 'users/show'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#landing'
+  resources :rooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

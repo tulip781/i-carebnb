cbb69f089c26cb85d9f65c3059406f361cd5da39
Raj Panasar <rbodderick@gmail.com>
2019-11-19 15:36:14 +0000

    schema and seeds fix a la Marcissimo

--- db/migrate/20191119103637_add_column_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnToUser < ActiveRecord::Migration[5.2]
-  def change
-      add_column :users, :start_date, :date
-      add_column :users, :end_date, :date
-
-  end
-end
--- db/migrate/20191119142944_remove_columns_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnsFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :start_date
-    remove_column :users, :end_date
-  end
-end

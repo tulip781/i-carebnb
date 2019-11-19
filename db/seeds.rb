7459337fc6ce255eca83ec487f088e8be8443790
Oliver Inglis <ollie@theinglistwins.com>
2019-11-19 14:38:07 +0000

    working

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,37 +21,21 @@ flats_images = ["https://raw.githubusercontent.com/lewagon/flats-boilerplate/mas
 "https://ksassets.timeincuk.net/wp/uploads/sites/56/2013/05/Blue-bedroom-ideas-1.jpg",
 "https://www.noaandnani.co.uk/images/hampshire-single-bed-frame-in-white-p919-6359_image.jpg"]
 
+puts "creating users"
 10.times do
+  user = User.create(
+    email: Faker::Internet.free_email,
+    title: ["Mr", "Mrs", "Miss"].sample,
+    first_name: Faker::Name.first_name,
+    surname: Faker::Name.last_name ,
+    address: Faker::Address.street_address ,
+    phone_number: Faker::PhoneNumber.phone_number,
+    date_of_birth: Faker::Date.backward(days: 400),
+    gender: ["Male", "Female"].sample ,
+    host: [true, false].sample,
+    )
 end
-
-
-
-# create_table "users", force: :cascade do |t|
-#     t.string "email", default: "", null: false
-#     t.string "encrypted_password", default: "", null: false
-#     t.string "reset_password_token"
-#     t.datetime "reset_password_sent_at"
-#     t.datetime "remember_created_at"
-#     t.datetime "created_at", null: false
-#     t.datetime "updated_at", null: false
-#     t.string "title"
-#     t.string "first_name"
-#     t.string "surname"
-#     t.text "address"
-#     t.text "phone_number"
-#     t.date "date_of_birth"
-#     t.text "gender"
-#     t.boolean "host"
-#     t.date "start_date"
-#     t.date "end_date"
-#     t.index ["email"], name: "index_users_on_email", unique: true
-#     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-#   end
-
-
-
-
-
+puts "creating "
 10.times do
   adult_space = rand(5)
   child_space = rand(2)
@@ -70,17 +54,3 @@ end
  room.save!
 end
 
-  # create_table "rooms", force: :cascade do |t|
-  #   t.string "address"
-  #   t.integer "adult_space"
-  #   t.integer "child_space"
-  #   t.integer "infant_space"
-  #   t.integer "beds"
-  #   t.integer "max_stay_length"
-  #   t.bigint "user_id"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.string "image_url"
-  #   t.string "title"
-  #   t.boolean "availability", default: true
-  #   t.index ["user_id"], name: "index_rooms_on_user_id"

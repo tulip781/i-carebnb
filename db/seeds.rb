39c407db62b010f75124755f51c756ac45c1c77d
Oliver Inglis <ollie@theinglistwins.com>
2019-11-19 14:25:42 +0000

    working on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,35 +7,80 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+Room.destroy_all
+
+User.destroy_all
+user1 = User.create
+
+flats_images = ["https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+"https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
+"https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg",
+"https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat4.jpg",
+"https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat5.jpg",
+"https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat6.jpg",
+"https://ksassets.timeincuk.net/wp/uploads/sites/56/2013/05/Blue-bedroom-ideas-1.jpg",
+"https://www.noaandnani.co.uk/images/hampshire-single-bed-frame-in-white-p919-6359_image.jpg"]
+
+10.times do
+end
+
+
+
+# create_table "users", force: :cascade do |t|
+#     t.string "email", default: "", null: false
+#     t.string "encrypted_password", default: "", null: false
+#     t.string "reset_password_token"
+#     t.datetime "reset_password_sent_at"
+#     t.datetime "remember_created_at"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#     t.string "title"
+#     t.string "first_name"
+#     t.string "surname"
+#     t.text "address"
+#     t.text "phone_number"
+#     t.date "date_of_birth"
+#     t.text "gender"
+#     t.boolean "host"
+#     t.date "start_date"
+#     t.date "end_date"
+#     t.index ["email"], name: "index_users_on_email", unique: true
+#     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+#   end
+
+
+
 
 
 10.times do
-  adult_space = rand(5),
-  child_space = rand(2),
-  infant_space = rand(1),
+  adult_space = rand(5)
+  child_space = rand(2)
+  infant_space = rand(1)
   room = Room.new(
     address: Faker::Address.street_address,
     adult_space: adult_space,
     child_space: child_space,
     infant_space: infant_space,
-    beds: :adult_space + :child_space,
+    beds: (adult_space + child_space + infant_space),
     max_stay_length: rand(5),
-
+    image_url: flats_images[rand(8)],
+    availability: [true, false].sample,
+    user: User.all.sample
    )
  room.save!
 end
 
-# INSTALL FAKER GEM IN GEM FILE
-    # t.string "address"
-    # t.integer "adult_space"
-    # t.integer "child_space"
-    # t.integer "infant_space"
-    # t.integer "beds"
-    # t.integer "max_stay_length"
-    # t.string "availability"
-    # t.bigint "user_id"
-    # t.datetime "created_at", null: false
-    # t.datetime "updated_at", null: false
-    # t.string "image_url"
-    # t.string "title"
-    # t.index ["user_id"], name: "index_rooms_on_user_id"
+  # create_table "rooms", force: :cascade do |t|
+  #   t.string "address"
+  #   t.integer "adult_space"
+  #   t.integer "child_space"
+  #   t.integer "infant_space"
+  #   t.integer "beds"
+  #   t.integer "max_stay_length"
+  #   t.bigint "user_id"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  #   t.string "image_url"
+  #   t.string "title"
+  #   t.boolean "availability", default: true
+  #   t.index ["user_id"], name: "index_rooms_on_user_id"

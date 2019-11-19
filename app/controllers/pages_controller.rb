2ec5354c0b92942810b34b7e944baee7e5a4654a
Oliver Inglis <ollie@theinglistwins.com>
2019-11-19 16:23:42 +0000

    Created the Search method in pages controller, removed the authentication from this page, created the view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:landing]
+  skip_before_action :authenticate_user!, only: [:landing, :search]
 
   def landing
   end
 
+  def search
+  end
 end

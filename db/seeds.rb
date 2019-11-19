0c477602d961489b36fa308b5d6dae4fc0e37181
Oliver Inglis <ollie@theinglistwins.com>
2019-11-19 13:47:30 +0000

    current seed progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+
+10.times do
+  adult_space = rand(5),
+  child_space = rand(2),
+  infant_space = rand(1),
+  room = Room.new(
+    address: Faker::Address.street_address,
+    adult_space: adult_space,
+    child_space: child_space,
+    infant_space: infant_space,
+    beds: :adult_space + :child_space,
+    max_stay_length: rand(5),
+
+   )
+ room.save!
+end
+
+# INSTALL FAKER GEM IN GEM FILE
+    # t.string "address"
+    # t.integer "adult_space"
+    # t.integer "child_space"
+    # t.integer "infant_space"
+    # t.integer "beds"
+    # t.integer "max_stay_length"
+    # t.string "availability"
+    # t.bigint "user_id"
+    # t.datetime "created_at", null: false
+    # t.datetime "updated_at", null: false
+    # t.string "image_url"
+    # t.string "title"
+    # t.index ["user_id"], name: "index_rooms_on_user_id"

aa52a35b2f17edb572b0335d19b902d0772f9a7f
Oliver Inglis <ollie@theinglistwins.com>
2019-11-20 15:46:54 +0000

    working map

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_11_20_125303) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,12 +43,11 @@ ActiveRecord::Schema.define(version: 2019_11_20_125303) do
     t.datetime "updated_at", null: false
     t.string "image_url"
     t.string "title"
-    t.float "latitude"
-    t.float "longitude"
     t.text "description"
     t.string "postcode"
     t.string "facilities"
-
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 

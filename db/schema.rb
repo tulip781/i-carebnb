9cfa1e42546b950672dfc0eebd3f02d27f42a107
Maria Herrera <mariluz.herrera@gmail.com>
2019-11-20 13:08:54 +0000

    change of rooms schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_151555) do
+ActiveRecord::Schema.define(version: 2019_11_20_125303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,9 @@ ActiveRecord::Schema.define(version: 2019_11_19_151555) do
     t.datetime "updated_at", null: false
     t.string "image_url"
     t.string "title"
-    t.boolean "availability", default: true
+    t.text "description"
+    t.string "postcode"
+    t.string "facilities"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 

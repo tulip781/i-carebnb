08a8ca9808af9444d381222dddab5b3fa1ddfee0
Maria Herrera <mariluz.herrera@gmail.com>
2019-11-25 16:35:38 +0000

    Remove title from empty sections and adding the sql queries to the controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,11 +12,17 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @rooms = current_user.rooms
+    @bookings = current_user.rooms.map { |room| room.bookings }.flatten
+    @booking_requests = @bookings.select { |booking| booking.confirmed == false && booking.declined == false }
+    # @requests = Booking.get_bookings(@rooms)
+    @charity_confirmed_bookings = current_user.bookings.where(confirmed: true)
+    @charity_declined_bookings = current_user.bookings.where(declined: true)
+    @charity_pending_bookings = current_user.bookings.select { |booking| booking.confirmed == false && booking.declined == false }
   end
 
   def switch_login
     session[:guest] = !session[:guest]
     redirect_to(request.env['HTTP_REFERER'])
   end
-
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  # def self.get_bookings(rooms)
+  #   @bookings = rooms.map { |r| r.bookings }
+  #   @selected_bookings = @bookings.select do |booking|
+  #     booking.confirmed == false && booking.declined == false
+  #   end
+  # end
 end

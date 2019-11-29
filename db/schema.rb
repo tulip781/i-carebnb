f75b13d1a62c9c8841638ae79fb6b4aa1a65434e
Ben Barton <benrussellbarton@gmail.com>
2019-11-29 11:39:56 +0000

    bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_11_28_150913) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,8 +135,6 @@ ActiveRecord::Schema.define(version: 2019_11_28_150913) do
     t.datetime "updated_at", null: false
     t.string "image_url"
     t.string "title"
-    t.float "latitude"
-    t.float "longitude"
     t.text "description"
     t.string "postcode"
     t.string "facilities"

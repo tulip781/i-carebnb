74ce2f69e7b526e6faa1d89329c79c0fa3c1a838
Raj Panasar <rbodderick@gmail.com>
2019-12-03 13:47:42 +0000

    chatroom getting closer

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,29 +2,32 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :room
   belongs_to :guest
-  # after_create :send_confirmation_message
+#   after_create :send_confirmation_message
 
-  # def send_confirmation_message
-  #   Chat.create(
-  #     @chat = Chat.new(
-  #       message: "Thank you very much! I have booked your room for the guest(s) we work with.  Please contact me if you have any questions.  Thank you for iCaringBnB.",)
+#   def send_confirmation_message
+#     Chatroom.find_or_initialize_by()
+# Chatroom.find_by(recipient: current_user, sender: @user) || Chatroom.find_by(recipient: @user, sender: current_user)
 
-  #     @chatrooms = current_user.chatrooms
-  #     @chat.chatroom = @chatroom
-  #     @chat.user = current_user
+#     Chat.create(
+#       @chat = Chat.new(
+#         message: "Thank you very much! I have booked your room for the guest(s) we work with.  Please contact me if you have any questions.  Thank you for iCaringBnB.",)
 
-  #     if @chat.save
-  #       Pusher.trigger('chat-channel', 'message-created', {
-  #         message: @chat.message,
-  #         sender_id: @chatroom.sender.id,
-  #         current_user: @chat.user.id,
-  #         timestamp: @chat.created_at.strftime('%I:%M %p'),
-  #         user_email: @chat.user.email
-  #       })
-  #       redirect_to chatroom_path(@chat.chatroom)
-  #     else
-  #       render 'chatrooms/show'
-  #     end
+#       @chatrooms = current_user.chatrooms
+#       @chat.chatroom = @chatroom
+#       @chat.user = current_user
+
+#       if @chat.save
+#         Pusher.trigger('chat-channel', 'message-created', {
+#           message: @chat.message,
+#           sender_id: @chatroom.sender.id,
+#           current_user: @chat.user.id,
+#           timestamp: @chat.created_at.strftime('%I:%M %p'),
+#           user_email: @chat.user.email
+#         })
+#         redirect_to chatroom_path(@chat.chatroom)
+#       else
+#         render 'chatrooms/show'
+#       end
   # end
 
 
